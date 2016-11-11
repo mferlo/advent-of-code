c7a08e62a9e7bfb81faa0f177742d7dfeed62120
Matt Ferlo <matt.ferlo@gmail.com>
2016-11-11 07:07:07 -0800

    6

--- 06/6.clj
+++ 06/6.clj
@@ -2,36 +2,37 @@
   (and (<= (:x upper-left) (:x cur) (:x lower-right))
        (<= (:y upper-left) (:y cur) (:y lower-right))))
 
-(defn to-2d [i]
-  {:x (quot i 1000) :y (rem i 1000)})
-
-(defn from-2d [x y]
-  {:x x :y y})
+(defn as-2d
+  ([i] {:x (quot i 1000) :y (rem i 1000)})
+  ([x y] {:x (Integer/parseInt x) :y (Integer/parseInt y)}))
   
-(defn get-func [s]
-  (case s
-    "toggle" #(if (= % 0) 1 0)
-    "turn on" 1
-    "turn off" 0))
+(defn get-func-for-line-a [s]
+  (let [[_ cmd x1 y1 x2 y2] (re-find #"^(.*) (\d+),(\d+) through (\d+),(\d+)$" (str s))]
+    (fn[index state]
+      (if (in-range? (as-2d x1 y1) (as-2d index) (as-2d x2 y2)) 
+        (case cmd
+          "toggle" (if (= 0 state) 1 0)
+          "turn on" 1
+          "turn off" 0)
+        state))))
 
-(defn parse-line [s]
-  (let [regex (re-find #"^(.*) (\d+),(\d+) through (\d+),(\d+)" (str s))]
-    {:func (get-func regex[1])
-      :upper-left (from-2d regex[2] regex[3])
-      :lower-right (from-2d regex[4] regex[5])}))
-  
-(defn make-next-lights [state index input]
-  (if (in-range? (:upper-left input) (to-2d index) (:lower-right input))
-    ((:func input) state)
-    (state)))
+(defn get-func-for-line-b [s]
+  (let [[_ cmd x1 y1 x2 y2] (re-find #"^(.*) (\d+),(\d+) through (\d+),(\d+)$" (str s))]
+    (fn[index state]
+      (if (in-range? (as-2d x1 y1) (as-2d index) (as-2d x2 y2)) 
+        (case cmd
+          "toggle" (+ state 2)
+          "turn on" (inc state)
+          "turn off" (max 0 (dec state)))
+        state))))
 
-(with-open [rdr (clojure.java.io/reader "input.txt")]
-  (->> (line-seq rdr)
-       parse-line
-;       count
-       println))
+(defn make-next-lights [state f]
+  (map-indexed f state))
 
-       ;; 
-       ;; (reduce-kv make-next-lights (repeat 1000000 0))
-       ;; (reduce +)
-       ;; println))
+; Correct, but naive (inefficient)
+(->> (slurp "input.txt")
+     clojure.string/split-lines
+     (map get-func-for-line-b)
+     (reduce make-next-lights (repeat 1000000 0))
+     (reduce +)
+     println)
