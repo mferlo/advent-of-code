fdc959a268366b2ec07d6b2483f806e382d08c9f
Matt Ferlo <matt.ferlo@gmail.com>
2016-12-01 20:19:23 -0800

    Utility script for creating answer skeleton

---
+++ generate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+i=$1
+zero_padded=$(seq -f '%02g' $i $i)
+
+mkdir $zero_padded
+cd $zero_padded
+echo "java -cp ../../clojure-1.8.0/clojure-1.8.0.jar clojure.main $i.clj" > run
+chmod +x run
+touch $i.clj
