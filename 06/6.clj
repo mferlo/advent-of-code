6aba00d5507e33d322494141f7d3b173488254d7
Matt Ferlo <mferlo@us.imshealth.com>
2016-12-06 14:09:08 -0800

    Problem 6: consolidate consecutive maps

--- 06/6.clj
+++ 06/6.clj
@@ -1,10 +1,6 @@
 (->> (slurp "input.txt")
      clojure.string/split-lines
      (apply map str)
-     (map frequencies)
-     (map #(sort-by val > %)) ; part 1
-     ; (map #(sort-by val < %)) ; part 2
-     (map first)
-     (map first)
+     (map #(->> (frequencies %) (sort-by val >) first first))
      (apply str)
      println)
