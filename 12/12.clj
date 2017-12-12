7fd712ac941ea3a4451f348f7ee85aae63949885
Matt Ferlo <mferlo@us.imshealth.com>
2017-12-12 15:11:26 -0800

    12 more cleanup

--- 12/12.clj
+++ 12/12.clj
@@ -9,17 +9,17 @@
 
 (defn filter-already-seen [seen s] (remove (partial contains? seen) s))
 
-(defn find-all-connected [to-examine seen graph]
+(defn find-all-connected [to-examine seen]
   (if-let [current (first to-examine)]
     (let [unseen-neighbors (filter-already-seen seen (graph current))]
-      (recur (concat (rest to-examine) unseen-neighbors) (conj seen current) graph))
+      (recur (concat (rest to-examine) unseen-neighbors) (conj seen current)))
     seen))
 
-(defn count-groups [num-groups seen graph]
+(defn count-groups [num-groups seen]
   (if-let [unvisited (not-empty (filter-already-seen seen (keys graph)))]
-    (let [visited (find-all-connected (list (first unvisited)) seen graph)]
-      (recur (inc num-groups) (into seen visited) graph))
+    (let [visited (find-all-connected (list (first unvisited)) seen)]
+      (recur (inc num-groups) (into seen visited)))
     num-groups))
 
-(println "Part 1: " (count (find-all-connected '("0") #{} graph)))
-(println "Part 2: " (count-groups 0 #{} graph))
+(println "Part 1: " (count (find-all-connected '("0") #{})))
+(println "Part 2: " (count-groups 0 #{}))
