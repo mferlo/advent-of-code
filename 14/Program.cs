e10ecf5275a55f8f407ad39122aa0a818a1cf031
Matt Ferlo <mferlo@us.imshealth.com>
2017-12-15 11:19:35 -0800

    14b

--- 14/Program.cs
+++ 14/Program.cs
@@ -87,10 +87,45 @@ namespace _14 {
             return result;
         }
 
+        static IEnumerable<(int x, int y)> Neighbors(int x, int y) {
+            if (x > 0 && y > 0)     yield return (x - 1, y - 1);
+            if (x > 0 && y < 127)   yield return (x - 1, y + 1);
+            if (x < 127 && y > 0)   yield return (x + 1, y - 1);
+            if (x < 127 && y < 127) yield return (x + 1, y + 1);
+        }
+
+        static void RemoveGroup(bool[,] bits, int x, int y) {
+            var toProcess = new Queue<(int x, int y)>();
+            toProcess.Enqueue((x, y));
+            while (toProcess.Any()) {
+                var cur = toProcess.Dequeue();
+                bits[cur.x, cur.y] = false;
+                foreach (var n in Neighbors(cur.x, cur.y).Where(p => bits[p.x, p.y])) {
+                    toProcess.Enqueue(n);
+                }
+            }
+        }
+
+        static int CountGroups(bool[,] bits) {
+            var groups = 0;
+            for (int x = 0; x < 128; x++) {
+                for (int y = 0; y < 128; y++) {
+                    if (bits[x, y]) {
+                        groups++;
+                        RemoveGroup(bits, x, y);
+                    }
+                }
+            }
+            return groups;
+        }
+
         static void Main(string[] args) {
             var bits = GetBits();
             var part1 = bits.Cast<bool>().Count(b => b);
             Console.WriteLine(part1);
+
+            var part2 = CountGroups(bits);
+            Console.WriteLine(part2);
         }
     }
 }
