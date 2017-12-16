7f70d79c5f2415eb455085e886efb6c2e42e197c
Matt Ferlo <matt.ferlo@gmail.com>
2017-12-16 15:05:35 -0800

    16

--- 16/16.clj
+++ 16/16.clj
@@ -1,33 +1,29 @@
-(defn spin [s x]
-  (vec (concat (take-last x s) (take (- (count s) x) s))))
-
-(defn exchange [s a b]
-  (assoc s a (s b) b (s a)))
-
-(defn partner [s a b]
-  (let [aa (.indexOf s a)
-        bb (.indexOf s b)]
-    (assoc s aa (s bb) bb (s aa))))
-
-(def init (vec "abcdefghijklmnop"))
+(defn spin [x s] (vec (concat (take-last x s) (take (- (count s) x) s))))
+(defn exchange [a b s] (assoc s a (s b) b (s a)))
+(defn partner [a b s] (exchange (.indexOf s a) (.indexOf s b) s))
 
 (defn parse [step]
   (let [op (first step)
-        args (apply str (rest step))
-        [a1 a2] (clojure.string/split args #"/")]
+        [a1 a2] (clojure.string/split (apply str (rest step)) #"/")]
     (condp = op
-      \p { :op :p :a1 (first a1) :a2 (first a2) }
-      \x { :op :x :a1 (Integer/parseInt a1) :a2 (Integer/parseInt a2) }
-      \s { :op :s :a1 (Integer/parseInt a1) })))
+      \p (partial partner (first a1) (first a2))
+      \x (partial exchange (Integer/parseInt a1) (Integer/parseInt a2))
+      \s (partial spin (Integer/parseInt a1)))))
 
+(def init "abcdefghijklmnop")
 (def instructions (map parse (clojure.string/split (slurp "input") #",")))
 
-(defn exec-step [s step]
-  (condp = (:op step)
-    :p (partner s (:a1 step) (:a2 step))
-    :x (exchange s (:a1 step) (:a2 step))
-    :s (spin s (:a1 step))))
+(defn dance
+  ([s] (apply str (reduce #(%2 %1) (vec s) instructions)))
+  ([s n] (if (zero? n) s (recur (dance s) (dec n)))))
 
-(defn exec [s] (reduce exec-step s instructions))
+(println "Part 1: " (dance init))
 
-(println "Part 1: " (apply str (exec init)))
+(defn cycle-length [s seen step]
+  (if (contains? seen s)
+    step
+    (recur (dance s) (assoc seen s step) (inc step))))
+
+(let [cycle (cycle-length init {} 0)
+      steps-to-do (mod 1000000000 cycle)]
+  (println "Part 2: " (dance init steps-to-do)))
