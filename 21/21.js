82a0d4898be33e74640a3beb1f4ebc0b2a5ee588
Matt Ferlo <matt.ferlo@gmail.com>
2017-12-21 19:02:31 -0800

    21 stub for doing the step

--- 21/21.js
+++ 21/21.js
@@ -1,86 +1,61 @@
 'use strict';
 
-/* 01 --> 20
-   23     31 */
-const rotate2 = (rows) =>
-  rows[1][0] + rows[0][0] + '/' +
-  rows[1][1] + rows[0][1];
-
-/* 012     630
-   345 --> 741
-   678     852 */
-const rotate3 = (rows) =>
-  rows[2][0] + rows[1][0] + rows[0][0] + '/' +
-  rows[2][1] + rows[1][1] + rows[0][1] + '/' +
-  rows[2][2] + rows[1][2] + rows[0][2];
-
-const rotate = (s, n) => {
-  switch (n) {
-    case 2: return rotate2(s.split('/'));
-    case 3: return rotate3(s.split('/'));
-    default: throw `WTF: ${n}`;
-  }
-};
-
-/* 01 --> 10
-   23     32 */
-const flip2 = (rows) =>
-  rows[0][1] + rows[0][0] + '/' +
-  rows[1][1] + rows[1][0];
-
-/* 012     210
-   345 --> 543
-   678     876 */
-const flip3 = (rows) =>
-  rows[0][2] + rows[0][1] + rows[0][0] + '/' +
-  rows[1][2] + rows[1][1] + rows[1][0] + '/' +
-  rows[2][2] + rows[2][1] + rows[2][0];
-
-const flip = (s, n) => {
-  switch (n) {
-    case 2: return flip2(s.split('/'));
-    case 3: return flip3(s.split('/'));
-    default: throw `WTF: ${n}`;
-  }
-};
-
-const parseLine = (line) => {
-  const [from, to] = line.split(' => ');
-  const n = from.split('/')[0].length;
-
-  const result = { n, to };
-
-  const permutations = [ from, flip(from, n) ];
-  let c = from;
-  for (let i = 0; i < 3; i++) {
-    c = rotate(c, n);
-    const f = flip(c, n);
-    permutations.push(c, f);
-  }
-  return { n, to, permutations };
-};
-
-const makeTransforms = (lines) => {
-  const result = [ {}, {}, {}, {} ];
-  for (const line of lines) {
-    const { n, to, permutations } = parseLine(line);
-    const size = result[n];
-    for (const p of permutations) {
-      size[p] = to;
-    }
-  }
-  return result;
-};
-
 const transforms = makeTransforms(lines);
 
+const initialGrid = '.#./..#/###';
+let grid;
+
+const display = () =>
+  document.getElementById('grid').innerHTML =
+    grid.replace(new RegExp('/', 'g'), '<br />');
+
 const init = () => {
-  const grid = document.getElementById('grid');
-  grid.innerHTML = "Hello,<br /> world!";
-  console.log(transforms[2]);
-  console.log(transforms[3]);
+  grid = initialGrid;
+  display();
 };
 
-const go = () => {
-  alert('!');
+// const asString = (g) => g.map(r => r.join('')).join('/');
+
+/* splitSubGrids3:
+   * put in 2d array `g`
+   * for (i = 0; i < n; i += 3)
+   *   for (j = 0; j < n; j += 3)
+   *     yield return asString(g[i + 0,1,2][j + 0,1,2])
+   */
+
+const splitSubGrids2 = (grid, n) => grid;
+const splitSubGrids3 = (grid, n) => grid;
+
+/* joinSubGrids3:
+   * create 2d array `g`
+   * for (i = 0; i < n; i += 3)
+   *   for (j = 0; j < n; j += 3)
+   *     g[i + 0,1,2][j + 0,1,2] = subgrid[i][j]
+   * return asString(g)
+   */
+
+const joinSubGrids2 = (subGrids, n) => subGrids;
+const joinSubGrids3 = (subGrids, n) => subGrids;
+
+const step = () => {
+  document.getElementById('count').innerHTML++;
+
+  const n = len(grid);
+  let subGrids, nextN;
+  if (n % 2 === 0) {
+    nextN = n * 3/2;
+    subGrids = splitSubGrids2(grid, n);
+  } else {
+    nextN = n * 4/3;
+    subGrids = splitSubGrids3(grid, n);
+  }
+
+  const nextSubGrids = subGrids;
+//  const nextSubGrids = subGrids.map(sg => transforms[sg][to]);
+
+  if (n % 2 === 0) {
+    grid = joinSubGrids3(nextSubGrids, nextN);
+  } else {
+    grid = joinSubGrids2(nextSubGrids, nextN);
+  }
 };
