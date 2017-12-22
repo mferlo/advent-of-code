e53a1842294652f6b32f3cd64b2aaab88200573b
Matt Ferlo <matt.ferlo@gmail.com>
2017-12-22 12:50:54 -0800

    21

--- 21/21.js
+++ 21/21.js
@@ -1,139 +1,60 @@
 'use strict';
 
-const transforms = makeTransforms(lines);
+let grid, count;
+const initialGrid = ['.#.', '..#', '###' ];
 
-const initialGrid = '.#./..#/###';
-let grid = initialGrid;
-
-const display = () =>
-  document.getElementById('grid').innerHTML =
-    grid.replace(new RegExp('/', 'g'), '<br />');
-
-const init = () => display();
-
-const asString = (g) => g.map(r => r.join('')).join('/');
-
-const to2dArray = (grid, n) => {
-  const g = new Array(n);
-  const rows = grid.split('/');
-  for (let i = 0; i < n; i++) {
-    g[i] = rows[i].split('');
-  }
-  return g;
+const byId = (id) => document.getElementById(id);
+const display = () => {
+  byId('grid').innerHTML = grid.join('<br />');
+  byId('count').innerHTML = count;
 };
 
-const gridStringN = (g, n, iIn, jIn) => {
-  console.log('-> gridStringN', g, n, iIn, jIn);
-  let result = "";
-  for (let i = iIn; i < iIn + n; i++) {
-    for (let j = jIn; j < jIn + n; j++) {
-      console.log('-> gridStringN', i, j, g[i]);
-      result += g[i].join('');
-    }
-    result += '/';
-  }
-  return result.slice(0, -1);
-}
-
-const gridString2 = (g, i, j) => gridStringN(g, 2, i, j);
-const gridString3 = (g, i, j) => gridStringN(g, 3, i, j);
-
-const splitSubGrids = (grid, n, subGridSize, subGridToString) => {
-  const g = to2dArray(grid, n);
-  const results = [];
-  for (let i = 0; i < n; i += subGridSize) {
-    for (let j = 0; j < n; j += subGridSize) {
-      results.push(subGridToString(g, i, j));
-    }
-  }
-  return results;
+const init = () => {
+  grid = initialGrid;
+  count = 0;
+  display();
 };
 
-
-const joinSubGrids4 = (subGrids, oldN, n) => {
-  console.log('joinSubGrids4', subGrids[0]);
-  if (subGrids.length === 1) {
-    return subGrids[0];
-  } else {
-    throw "FIXME joinSubGrids4 n > 1";
+const getSubGrid = (i, j, n) => {
+  const result = [];
+  for (let row = 0; row < n; row++) {
+    result[row] = grid[i+row].slice(j, j+n);
   }
-};
-/*
-  const g = new Array(n);
-  for (let i = 0; i < n; i++) {
-    g[i] = new Array(n);
-  }
-
-  let r = 0;
-  let c = 0;
-  for (let subGrid of subGrids) {
-    const lines = subGrid.split('/');
-
-    c += oldN;
-    if (c >= oldN) {
-      c = 0;
-      r += oldN;
-    }
-  }
-*/
-
-/* joinSubGrids3:
-   * create 2d array `g`
-   * for (i = 0; i < n; i += 3)
-   *   for (j = 0; j < n; j += 3)
-   *     g[i + 0,1,2][j + 0,1,2] = subgrid[i][j]
-   * return asString(g)
-   */
-const joinSubGridRow = (subGrids, row) => {
-  subGrids.map(sg => sg[row].replace('/', ''));
-};
-
-const joinSubGrids3 = (subGrids, n) => {
-  const subGridsPerRow = n / 3;
-  console.log('joinSubGrids3', n, subGrids, subGridsPerRow);
-  if (subGridsPerRow === 2) {
-    let row = subGrids.slice(0, 2).map(sg => to2dArray(sg, 3));
-    console.log('slice(0,2)', row);
-    row = subGrids.slice(2, 2).map(sg => to2dArray(sg, 3));
-    console.log('slice(2,2)', row);
-  } else {
-    throw "FIXME";
-  }
-
-  let result = "";
   return result;
-//  for (let i = 0; i < subGridsPerRow; i++) {
-//    const subGridRow = subGrids.slice(3*i, 3*(i+1));
-//
-//    const r0 = subGrids.map(sg => sg[0].replace(new RegExp('/', 'g'), ''))
-//  }
+};
+
+const populateNewGrid = (i, n, subGrid, newGrid) => {
+  for (let row = 0; row < n; row++) {
+    newGrid[i+row] += subGrid[row];
+  }
 };
 
 const step = () => {
-  console.log(grid);
-  const n = len(grid);
+  const n = grid[0].length;
+  const oldSize = n % 2 === 0 ? 2 : 3;
+  const newSize = n % 2 === 0 ? 3 : 4;
+  const numCells = n / oldSize;
 
-  let subGridSize, subGridToString, nextN;
-  if (n % 2 === 0) {
-    subGridSize = 2;
-    subGridToString = gridString2;
-    nextN = n * 3/2;
-  } else {
-    subGridSize = 3;
-    subGridToString = gridString3;
-    nextN = n * 4/3;
+  const newGrid = Array(n * newSize / oldSize).fill("");
+  for (let i = 0; i < numCells; i++) {
+    for (let j = 0; j < numCells; j++) {
+      const oldSubGrid = getSubGrid(i*oldSize, j*oldSize, oldSize);
+      const newSubGrid = transform(oldSubGrid);
+      populateNewGrid(i*newSize, newSize, newSubGrid, newGrid);
+    }
   }
-  const subGrids = splitSubGrids(grid, n, subGridSize, subGridToString);
-  const nextSubGrids = subGrids.map(sg => transforms[subGridSize][sg]);
-
-  if (n % 2 === 0) {
-    grid = joinSubGrids3(nextSubGrids, nextN);
-  } else {
-    grid = joinSubGrids4(nextSubGrids, n, nextN);
-  }
-  console.log(grid);
-
-
-  document.getElementById('count').innerHTML++;
+  grid = newGrid;
+  count++;
   display();
 };
+
+const runN = (n) => {
+  init();
+  for (let i = 0; i < n; i++) {
+    step();
+  }
+  byId('answer').innerHTML = grid.join('').match(/#/g).length;
+};
+
+const part1 = () => runN(5);
+const part2 = () => runN(18);
--- 21/parse.js
+++ 21/parse.js
@@ -1,21 +1,25 @@
+/* transform() accepts a 2x2 (['12','34']) or 3x3 (['123','456','789']) grid.
+   It will output the transform of that grid in the same format
+   (3x3 or 4x4, respectively), accounting for rotation or reflection. */
+
 /* 01 --> 20
    23     31 */
 const rotate2 = (rows) =>
-  rows[1][0] + rows[0][0] + '/' +
-  rows[1][1] + rows[0][1];
+  [ rows[1][0] + rows[0][0],
+    rows[1][1] + rows[0][1] ];
 
 /* 012     630
    345 --> 741
    678     852 */
 const rotate3 = (rows) =>
-  rows[2][0] + rows[1][0] + rows[0][0] + '/' +
-  rows[2][1] + rows[1][1] + rows[0][1] + '/' +
-  rows[2][2] + rows[1][2] + rows[0][2];
+  [ rows[2][0] + rows[1][0] + rows[0][0],
+    rows[2][1] + rows[1][1] + rows[0][1],
+    rows[2][2] + rows[1][2] + rows[0][2] ];
 
-const rotate = (s, n) => {
+const rotate = (rows, n) => {
   switch (n) {
-    case 2: return rotate2(s.split('/'));
-    case 3: return rotate3(s.split('/'));
+    case 2: return rotate2(rows);
+    case 3: return rotate3(rows);
     default: throw `WTF: ${n}`;
   }
 };
@@ -23,31 +27,28 @@ const rotate = (s, n) => {
 /* 01 --> 10
    23     32 */
 const flip2 = (rows) =>
-  rows[0][1] + rows[0][0] + '/' +
-  rows[1][1] + rows[1][0];
+  [ rows[0][1] + rows[0][0],
+    rows[1][1] + rows[1][0] ];
 
 /* 012     210
    345 --> 543
    678     876 */
 const flip3 = (rows) =>
-  rows[0][2] + rows[0][1] + rows[0][0] + '/' +
-  rows[1][2] + rows[1][1] + rows[1][0] + '/' +
-  rows[2][2] + rows[2][1] + rows[2][0];
+  [ rows[0][2] + rows[0][1] + rows[0][0],
+    rows[1][2] + rows[1][1] + rows[1][0],
+    rows[2][2] + rows[2][1] + rows[2][0] ];
 
-const flip = (s, n) => {
+const flip = (rows, n) => {
   switch (n) {
-    case 2: return flip2(s.split('/'));
-    case 3: return flip3(s.split('/'));
+    case 2: return flip2(rows);
+    case 3: return flip3(rows);
     default: throw `WTF: ${n}`;
   }
 };
 
-const len = (g) => g.split('/')[0].length;
-
 const parseLine = (line) => {
-  const [from, to] = line.split(' => ');
-  const n = len(from);
-
+  const [from, to] = line.split(' => ').map(l => l.split('/'));
+  const n = from[0].length;
   const result = { n, to };
 
   const permutations = [ from, flip(from, n) ];
@@ -66,8 +67,12 @@ const makeTransforms = (lines) => {
     const { n, to, permutations } = parseLine(line);
     const size = result[n];
     for (const p of permutations) {
-      size[p] = to;
+      size[JSON.stringify(p)] = to;
     }
   }
   return result;
 };
+
+const transforms = makeTransforms(lines);
+
+const transform = (grid) => transforms[grid[0].length][JSON.stringify(grid)];
