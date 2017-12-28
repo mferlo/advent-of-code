1554943ea4804a47f1615d0506c5e7701c6fe654
Matt Ferlo <matt.ferlo@gmail.com>
2017-12-27 17:03:26 -0800

    22

--- 22/Program.cs
+++ 22/Program.cs
@@ -12,21 +12,25 @@ namespace _22 {
         private (int x, int y) cur;
         private D dir;
         private ImmutableHashSet<(int x, int y)> infected;
-        private int numInfected;
+        private ImmutableHashSet<(int x, int y)> weakened;
+        private ImmutableHashSet<(int x, int y)> flagged;
 
         public Map(IEnumerable<(int x, int y)> initialInfected) {
             cur = (0, 0);
             dir = D.Up;
             infected = ImmutableHashSet<(int x, int y)>.Empty
                 .Union(initialInfected);
-            numInfected = 0;
+            weakened = ImmutableHashSet<(int x, int y)>.Empty;
+            flagged = ImmutableHashSet<(int x, int y)>.Empty;
         }
 
-        public int Part1 => numInfected;            
         private bool IsInfected => infected.Contains(cur);
+        private bool IsWeakened => weakened.Contains(cur);
+        private bool IsFlagged => flagged.Contains(cur);
 
-        private D Left() => (D)(((int)dir + 3) % 4);
         private D Right() => (D)(((int)dir + 1) % 4);
+        private D UTurn() => (D)(((int)dir + 2) % 4);
+        private D Left()  => (D)(((int)dir + 3) % 4);
 
         private (int x, int y) Move() {
             switch (dir) {
@@ -38,16 +42,43 @@ namespace _22 {
             }
         }
 
-        public void Act() {
-            if (IsInfected) {
-                dir = Right();
-                infected = infected.Remove(cur);
-            } else {
-                dir = Left();
-                infected = infected.Add(cur);
-                numInfected += 1;
+        public int Part1() {
+            var numInfected = 0;
+            for (var i = 0; i < 10000; i++) {
+                if (IsInfected) {
+                    dir = Right();
+                    infected = infected.Remove(cur);
+                } else {
+                    dir = Left();
+                    infected = infected.Add(cur);
+                    numInfected += 1;
+                }
+                cur = Move();
             }
-            cur = Move();
+            return numInfected;
+        }
+
+        public int Part2() {
+            var numInfected = 0;
+            for (var i = 0; i < 10000000; i++) {
+                if (IsWeakened) {
+                    weakened = weakened.Remove(cur);
+                    infected = infected.Add(cur);
+                    numInfected += 1;
+                } else if (IsInfected) {
+                    dir = Right();
+                    infected = infected.Remove(cur);
+                    flagged = flagged.Add(cur);
+                } else if (IsFlagged) {
+                    dir = UTurn();
+                    flagged = flagged.Remove(cur);
+                } else {
+                    dir = Left();
+                    weakened = weakened.Add(cur);
+                }
+                cur = Move();
+            }
+            return numInfected;
         }
     }
 
@@ -57,7 +88,6 @@ namespace _22 {
             var max = n / 2;
 
             var y = max;
-
             foreach (var line in lines) {
                 for (var x = 0; x < n; x++) {
                     if (line[x] == '#') {
@@ -69,36 +99,20 @@ namespace _22 {
         }
 
         static void Test() {
-            var map = new Map(Parse(new[] { "..#", "#..", "..." }));
-            var i = 0;
-            while (true) {
-                map.Act();
-                i++;
-                if (i == 7) {
-                    Console.WriteLine("7: " + map.Part1);
-                }
+            var input = Parse(new[] { "..#", "#..", "..." });
+            Console.WriteLine("Expected: 5587");
+            Console.WriteLine("Actual:   " + new Map(input).Part1());
 
-                if (i == 70) {
-                    Console.WriteLine("70: " + map.Part1);
-                }
-
-                if (i == 10_000) {
-                    Console.WriteLine("10000: " + map.Part1);
-                    break;
-                }
-            }
-        }
-
-        static void Part1() {
-            var map = new Map(Parse(File.ReadLines("input")));
-            for (var i = 0; i < 10000; i++) {
-                map.Act();
-            }
-            Console.WriteLine(map.Part1);
+            Console.WriteLine("Expected: 2511944");
+            Console.WriteLine("Actual:   " + new Map(input).Part2());
         }
 
         static void Main(string[] args) {
-            Part1();
+            Test();
+
+            var input = Parse(File.ReadLines("input"));
+            Console.WriteLine("Part 1: " + new Map(input).Part1());
+            Console.WriteLine("Part 1: " + new Map(input).Part2());
         }
     }
 }
