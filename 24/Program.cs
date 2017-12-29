f15b9a4777c849fdf1faae202ece092c1cad5c85
Matt Ferlo <matt.ferlo@gmail.com>
2017-12-29 15:49:16 -0800

    24

--- 24/Program.cs
+++ 24/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 
 namespace _24 {
@@ -9,12 +10,14 @@ namespace _24 {
         public int p1 { get; }
         public int p2 { get; }
 
-        public Component(int p1, int p2) {
-            this.p1 = p1;
-            this.p2 = p2;
+        public Component(string s) {
+            var x = s.Split('/');
+            this.p1 = Int32.Parse(x[0]);
+            this.p2 = Int32.Parse(x[1]);
         }
 
         public int Strength => p1 + p2;
+        public bool CanAttach(int port) => p1 == port || p2 == port;
         public override string ToString() => $"{p1}/{p2}";
     }
 
@@ -43,24 +46,54 @@ namespace _24 {
         }
 
         public int Strength => Components.Sum(c => c.Strength);
+        public int Length => Components.Count;
         public static Bridge Empty => new Bridge(ImmutableList<Component>.Empty);
-        public override string ToString() => $"{String.Join(", ", Components)} -> {ExposedPort}";
-
+        public override string ToString() => $"{Strength}: {String.Join(", ", Components)}";
     }
 
     class Program {
         const string test = "0/2\n2/2\n2/3\n3/4\n3/5\n0/1\n10/1\n9/10";
 
-        static Component C(int x, int y) => new Component(x, y);
+        static IImmutableSet<Component> Parse(string input) =>
+            ImmutableHashSet<Component>.Empty
+                .Union(input.Split('\n').Where(s => s != "").Select(line => new Component(line)));
 
+        static IEnumerable<Component> AvailableComponents(Bridge b, IEnumerable<Component> components) =>
+            components.Where(c => !b.Components.Contains(c)).Where(c => c.CanAttach(b.ExposedPort));
+
+        static IEnumerable<Bridge> AddComponents(Bridge b, IEnumerable<Component> availableComponents) =>
+            availableComponents.Select(c => b.Add(c));
+
+        // Meh, good enough, but still kinda sad: "real 1m45.274s"
         static void Main(string[] args) {
-            var bridge = Bridge.Empty;
+            var strongest = Bridge.Empty;
+            var longest = Bridge.Empty;
 
-            bridge = bridge.Add(C(0, 1)).Add(C(10, 1)).Add(C(9, 10));
+            var components = Parse(File.ReadAllText("input")); // Parse(test);
+            var bridges = new Queue<Bridge>();
+            bridges.Enqueue(Bridge.Empty);
 
-            Console.WriteLine(bridge);
+            while (bridges.Any()) {
+                var b = bridges.Dequeue();
+                var available = AvailableComponents(b, components);
+                if (available.Any()) {
+                    foreach (var newBridge in AddComponents(b, available)) {
+                        bridges.Enqueue(newBridge);
+                    }
+                } else {
+                    if (strongest.Strength < b.Strength) {
+                        strongest = b;
+                    }
 
-            Console.WriteLine(bridge.Strength);
+                    if (longest.Length < b.Length ||
+                        (longest.Length == b.Length && longest.Strength < b.Strength)) {
+                        longest = b;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Strongest: {strongest}");
+            Console.WriteLine($"Longest: {longest}");
         }
     }
 }
