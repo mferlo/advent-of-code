950860e176b7f45e449bc79591ac828fea115200
Matt Ferlo <mferlo@us.imshealth.com>
2019-10-15 14:17:57 -0700

    Day 15 Part 1 - Test cases work but infinite loop?

--- 15/Program.cs
+++ 15/Program.cs
@@ -5,12 +5,45 @@ using System.Linq;
 namespace _15
 {
     enum UnitType { Elf, Goblin }
-    
+    enum CombatStatus { InProgress, Done }
+
+    struct Position
+    {
+        public int Row;
+        public int Col;
+
+        public Position(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public override string ToString() => $"{Row}, {Col}";
+
+        public IEnumerable<Position> AdjacentPositions() // In reading order
+        {
+            yield return new Position(Row - 1, Col);
+            yield return new Position(Row, Col - 1);
+            yield return new Position(Row, Col + 1);
+            yield return new Position(Row + 1, Col);
+        }
+
+        public static bool operator ==(Position lhs, Position rhs) => lhs.Row == rhs.Row && lhs.Col == rhs.Col;
+        public static bool operator !=(Position lhs, Position rhs) => !(lhs == rhs);
+    }
+
     class Unit
     {
-        public Unit(char type)
+        public UnitType Type { get; }
+        public Position Position { get; set; }
+
+        int AttackPower { get; }
+        public int HP { get; private set; }
+
+        public Unit(char type, Position position)
         {
             Type = type == 'E' ? UnitType.Elf : UnitType.Goblin;
+            Position = position;
             AttackPower = 3;
             HP = 200;
         }
@@ -25,26 +58,24 @@ namespace _15
             enemy.HP -= this.AttackPower;
         }
 
-        public UnitType Type { get; }
-        int AttackPower { get; }
-        int HP { get; set; }
+        public override string ToString() =>
+            $"{Type} ({Position}) - AP: {AttackPower}, HP: {HP}";
     }
 
     class GridCell
     {
-        int Row { get; }
-        int Col { get; }
+        public Position Position { get; }
         bool IsWall { get; }
-        Unit Occupant { get; set; }
+        public Unit Occupant { get; set; }
+        public bool IsEmpty => !IsWall && Occupant == null;
 
         public GridCell(char occupant, int row, int col)
         {
-            Row = row;
-            Col = col;
+            Position = new Position { Row = row, Col = col };
             IsWall = occupant == '#';
             if (occupant == 'E' || occupant == 'G')
             {
-                Occupant = new Unit(occupant);
+                Occupant = new Unit(occupant, Position);
             }
             else
             {
@@ -52,24 +83,18 @@ namespace _15
             }
         }
 
-        public override string ToString()
-        {
-            if (Occupant == null)
-            {
-                return IsWall ? "#" : ".";
-            }
-            else
-            {
-                return Occupant.Type == UnitType.Elf ? "E" : "G";
-            }
-        }
+        public bool IsOccupiedByEnemyOf(UnitType type) =>
+            Occupant != null && Occupant.Type != type;
+
+        public override string ToString() =>
+            Occupant == null ? (IsWall ? "#" : ".") : (Occupant.Type == UnitType.Elf ? "E" : "G");
     }
 
     class Grid
     {
-        private int NumRows;
-        private int NumCols;
-        private GridCell[][] Cells;
+        public int NumRows { get; }
+        public int NumCols { get; }
+        public GridCell[][] Cells;
 
         public Grid(IList<string> mapRows)
         {
@@ -86,6 +111,112 @@ namespace _15
             }
         }
 
+        GridCell Cell(Position position) => Cells[position.Row][position.Col];
+
+        public void Move(Unit unit, Position destination)
+        {
+            Cell(unit.Position).Occupant = null;
+            Cell(destination).Occupant = unit;
+            unit.Position = destination;
+        }
+
+        public IEnumerable<GridCell> InReadingOrder(Func<GridCell, bool> predicate) =>
+            InReadingOrder(predicate, cell => cell);
+
+        public IEnumerable<T> InReadingOrder<T>(Func<GridCell, bool> predicate, Func<GridCell, T> accessor)
+        {
+            for (int r = 0; r < NumRows; r++)
+            {
+                for (int c = 0; c < NumCols; c++)
+                {
+                    var cell = Cells[r][c];
+                    if (predicate(cell))
+                    {
+                        yield return accessor(cell);
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Unit> AdjacentEnemies(Unit unit) =>
+            unit.Position.AdjacentPositions().Where(p => IsValid(p) && Cell(p).IsOccupiedByEnemyOf(unit.Type)).Select(p => Cell(p).Occupant);
+
+        public IEnumerable<Position> ClosestPositions(Position from, ISet<Position> to)
+        {
+            var visited = new bool[NumRows][];
+            for (int r = 0; r < NumRows; r++)
+            {
+                visited[r] = new bool[NumCols];
+            }
+
+            var currentPositions = new List<Position> { from };
+
+            while (currentPositions.Any())
+            {
+                foreach (var p in currentPositions)
+                {
+                    visited[p.Row][p.Col] = true;
+                }
+
+                var adjacentPositions = currentPositions.SelectMany(p => p.AdjacentPositions())
+                    .Where(p => IsValid(p) && Cell(p).IsEmpty && !visited[p.Row][p.Col]);
+                
+                var foundPositions = to.Intersect(adjacentPositions);
+                if (foundPositions.Any())
+                {
+                    return foundPositions;
+                }
+
+                currentPositions = adjacentPositions.ToList();
+            }
+
+            return Enumerable.Empty<Position>();
+        }
+
+        public Position NextStepOnShortestPath(Position from, Position to)
+        {
+            // Go backwards, recording distance
+            var distance = new int?[NumRows][];
+            for (int r = 0; r < NumRows; r++)
+            {
+                distance[r] = new int?[NumCols];
+            }
+
+            distance[to.Row][to.Col] = 0;
+            var currentPositions = new List<Position> { to };
+            int steps = 0;
+
+            while (distance[from.Row][from.Col] == null)
+            {
+                steps++;
+                var adjacentPositions = currentPositions.SelectMany(p => p.AdjacentPositions())
+                    .Where(p => IsValid(p) && distance[p.Row][p.Col] == null && (p == from || Cell(p).IsEmpty))
+                    .ToList();
+                
+                foreach (var p in adjacentPositions)
+                {
+                    distance[p.Row][p.Col] = steps;
+                }
+
+                currentPositions = adjacentPositions;
+            }
+
+            return from.AdjacentPositions().First(p => steps - 1 == distance[p.Row][p.Col]);
+        }
+
+        public void Kill(Unit unit)
+        {
+            if (unit.HP > 0)
+            {
+                throw new Exception($"{unit} is not dead yet");
+            }
+            Cell(unit.Position).Occupant = null;
+        }
+
+
+        public bool IsValid(Position position) =>
+            position.Row >= 0 && position.Row < NumRows && position.Col >= 0 && position.Col < NumCols;
+
         private string StringifyRow(GridCell[] row) => String.Join("", row.Select(cell => cell.ToString()));
 
         public override string ToString() => String.Join("\n", Cells.Select(StringifyRow));
@@ -93,7 +224,104 @@ namespace _15
 
     class Program
     {
-        static string TestData = @"#########
+        static IEnumerable<Unit> GetUnitOrder(Grid grid) =>
+            grid.InReadingOrder(cell => cell.Occupant != null, cell => cell.Occupant);
+
+        static IEnumerable<Unit> FindTargets(Grid grid, Unit current) =>
+            grid.InReadingOrder(cell => cell.IsOccupiedByEnemyOf(current.Type), cell => cell.Occupant);
+
+        static IEnumerable<Position> GetAdjacentSpaces(Grid grid, IEnumerable<Unit> units)
+        {
+            foreach (var unit in units)
+            {
+                foreach (var adjacentSpace in unit.Position.AdjacentPositions())
+                {
+                    if (grid.IsValid(adjacentSpace))
+                    {
+                        yield return adjacentSpace;
+                    }
+                }
+            }
+        }
+
+        static Position? FindNextStep(Grid grid, Unit unit, ISet<Position> adjacentTargetSpaces)
+        {
+            var emptyAdjacentTargetPositions = grid.InReadingOrder(cell => adjacentTargetSpaces.Contains(cell.Position) && cell.IsEmpty)
+                .Select(c => c.Position)
+                .ToHashSet();
+            var closestEmptyAdjacentTargetSpaces = grid.ClosestPositions(unit.Position, emptyAdjacentTargetPositions);
+
+            if (!closestEmptyAdjacentTargetSpaces.Any()) // Can't make it to any spaces adjacent to an enemy
+            {
+                return null;
+            }
+
+            var spaceToMoveTowards = closestEmptyAdjacentTargetSpaces.OrderBy(p => p.Row).ThenBy(p => p.Col).First();
+            return grid.NextStepOnShortestPath(unit.Position, spaceToMoveTowards);
+        }
+
+        static void MaybeAttack(Grid grid, Unit unit)
+        {
+            var enemies = grid.AdjacentEnemies(unit);
+            if (!enemies.Any())
+            {
+                return;
+            }
+            var minHP = enemies.Min(e => e.HP);
+            var target = enemies.OrderBy(e => e.Position.Row).ThenBy(e => e.Position.Col).First(e => e.HP == minHP);
+            unit.Attack(target);
+            if (target.HP <= 0)
+            {
+                grid.Kill(target);
+            }
+        }
+
+        static CombatStatus TakeTurn(Grid grid, Unit unit)
+        {
+            if (unit.HP <= 0)
+            {
+                return CombatStatus.InProgress;
+            }
+
+            var targets = FindTargets(grid, unit).ToList();
+            if (!targets.Any())
+            {
+                return CombatStatus.Done;
+            }
+
+            var adjacentTargetSpaces = GetAdjacentSpaces(grid, targets).Distinct().ToHashSet();
+
+            if (!adjacentTargetSpaces.Contains(unit.Position)) // We have to move
+            {
+                var nextStep = FindNextStep(grid, unit, adjacentTargetSpaces);
+                if (nextStep == null)
+                {
+                    return CombatStatus.InProgress;
+                }
+                grid.Move(unit, nextStep.Value);
+            }
+
+            MaybeAttack(grid, unit);
+
+            return CombatStatus.InProgress;
+        }
+
+        static CombatStatus DoRound(Grid grid)
+        {
+            var units = GetUnitOrder(grid).ToList();
+            foreach (var unit in units)
+            {
+                if (TakeTurn(grid, unit) == CombatStatus.Done)
+                {
+                    return CombatStatus.Done;
+                }
+            }
+
+            return CombatStatus.InProgress;
+        }
+
+        private const string TestData =
+@"#########
 #G..G..G#
 #.......#
 #.......#
@@ -103,10 +331,77 @@ namespace _15
 #G..G..G#
 #########";
 
+        private const string TestCombat =
+@"#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######";
+
+        private const string TestCombat2 =
+@"#######
+#G..#E#
+#E#E.E#
+#G.##.#
+#...#E#
+#...E.#
+#######";
+
+        private const string Input = 
+@"################################
+##########..........############
+########G..................#####
+#######..G.GG...............####
+#######....G.......#......######
+########.G.G...............#E..#
+#######G.................#.....#
+########.......................#
+########G.....G....#.....##....#
+########.....#....G.........####
+#########..........##....E.E#.##
+##########G..G..........#####.##
+##########....#####G....####E.##
+######....G..#######.....#.....#
+###....#....#########......#####
+####........#########..E...#####
+###.........#########......#####
+####G....G..#########......#####
+####..#.....#########....#######
+######.......#######...E.#######
+###.G.....E.G.#####.....########
+#.....G........E.......#########
+#......#..#..####....#.#########
+#...#.........###.#..###########
+##............###..#############
+######.....E####..##############
+######...........###############
+#######....E....################
+######...####...################
+######...###....################
+###.....###..##..###############
+################################";
+
         static void Main(string[] args)
         {
-            var grid = new Grid(TestData.Split("\r\n"));
+            var grid = new Grid(Input.Split("\r\n"));
+            var rounds = 0;
+            while (DoRound(grid) != CombatStatus.Done)
+            {
+                rounds++;
+                Console.WriteLine(rounds);
+                Console.WriteLine(grid);
+                Console.WriteLine(String.Join("\n", GetUnitOrder(grid)));
+            }
+
+            Console.WriteLine(rounds);
             Console.WriteLine(grid);
+            Console.WriteLine(String.Join("\n", GetUnitOrder(grid)));
+
+            var hpRemaining = GetUnitOrder(grid).Sum(u => u.HP);
+            Console.WriteLine($"*** Result: {hpRemaining * rounds}");
+
         }
     }
 }
