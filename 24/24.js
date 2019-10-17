8c522faf8e89822ba2e72ffda6796162d1f084e4
Matt Ferlo <matt.ferlo@gmail.com>
2019-10-16 22:37:54 -0700

    Day 24 Targeting

--- 24/24.js
+++ 24/24.js
@@ -13,13 +13,13 @@ class Group {
     this.attackType = g.attackType;
 
     if (g.specials && g.specials.includes('immune')) {
-      this.immunities = g.specials.match(/immune to ([^;)]*)/).slice(1);
+      this.immunities = g.specials.match(/immune to ([^;)]*)/)[1].split(', ');
     } else {
       this.immunities = [];
     }
 
     if (g.specials && g.specials.includes('weak')) {
-      this.weaknesses = g.specials.match(/weak to ([^;)]*)/).slice(1);
+      this.weaknesses = g.specials.match(/weak to ([^;)]*)/)[1].split(', ');
     } else {
       this.weaknesses = [];
     }
@@ -27,6 +27,16 @@ class Group {
 
   get effectivePower() {
     return this.units * this.attack;
+  }
+
+  calculateDamageTo(defender) {
+    if (defender.immunities.includes(this.attackType)) {
+      return 0;
+    } else if (defender.weaknesses.includes(this.attackType)) {
+      return 2 * this.effectivePower;
+    } else {
+      return this.effectivePower;
+    }
   }
 }
 
@@ -40,5 +50,51 @@ const infectionTestInput = [
   "4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4"
 ];
 
-const g = new Group(immuneTestInput[0]);
-console.log(g);
+const byPowerThenInitiative = (a, b) =>
+  (b.effectivePower - a.effectivePower) || (a.initiative - b.initiative);
+
+const chooseTarget = (attacker, defenders) => {
+  const damageDealt = defenders.map(
+    d => ({ defenderId: d.id, damage: attacker.calculateDamageTo(d) }));
+
+  console.log(attacker);
+  console.log(damageDealt);
+
+  const maxDamageDealt = Math.max(...damageDealt.map(d => d.damage));
+  console.log(maxDamageDealt);
+  const targetIds = damageDealt
+    .filter(d => d.damage === maxDamageDealt)
+    .map(d => d.defenderId);
+
+  if (targetIds.length && maxDamageDealt > 0) {
+    const targets = defenders
+      .filter(d => targetIds.includes(d.id))
+      .sort(byPowerThenInitiative);
+    return targets[0].id;
+  } else {
+    return undefined;
+  }
+};
+
+const chooseTargets = (attackers, defenders) => {
+  const result = {};
+  const sortedAttackers = [ ...attackers ].sort(byPowerThenInitiative);
+  let remainingDefenders = [ ...defenders ];
+
+  for (const attacker of sortedAttackers) {
+    const targetId = chooseTarget(attacker, remainingDefenders);
+    console.log(`${attacker.id} attacking ${targetId}`);
+
+    if (targetId !== undefined) {
+      result[attackers.id] = targetId;
+      remainingDefenders = remainingDefenders.filter(d => d.id !== targetId);
+    }
+  }
+  return result;
+};
+
+const immuneGroups = immuneTestInput.map(i => new Group(i));
+const infectionGroups = infectionTestInput.map(i => new Group(i));
+
+chooseTargets(immuneGroups, infectionGroups);
+chooseTargets(infectionGroups, immuneGroups);
