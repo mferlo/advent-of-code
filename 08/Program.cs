0e15cd6e65d4d4e98b073a3d8b6238d201823fae
Matt Ferlo <matt.ferlo@gmail.com>
2019-12-08 19:33:13 -0800

    Day 8 Part 2

--- 08/Program.cs
+++ 08/Program.cs
@@ -9,7 +9,6 @@ namespace _08
     {
         static IEnumerable<string> GetLayers(string image, int width, int height)
         {
-            image = image.Trim();
             var layerSize = width * height;
             Debug.Assert(image.Length % layerSize == 0);
             for (int layerStartIndex = 0; layerStartIndex < image.Length; layerStartIndex += layerSize)
@@ -35,13 +34,31 @@ namespace _08
             return targetLayer.Count(ch => ch == '1') * targetLayer.Count(ch => ch == '2');
         }
 
+        static char MergePixel(char top, char bottom) => top != '2' ? top : bottom;
+
+        static string MergeLayers(string top, string bottom) => String.Concat(top.Zip(bottom, MergePixel));
+
+        static string RenderImage(IEnumerable<string> layers) => layers.Aggregate(MergeLayers);
+
+        static char Pixel2Char(char pixel) => pixel == '0' ? ' ' : '#';
+
+        static void PrintLine(string line) => Console.WriteLine(String.Join("", line.Select(Pixel2Char)));
+
+        static void PrintImage(string image, int width, int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                PrintLine(image.Substring(i * width, width));
+            }
+        }
+
         static void Main(string[] args)
         {
-            var image = System.IO.File.ReadAllText("input.txt");
+            var image = System.IO.File.ReadAllText("input.txt").Trim();
             var layers = GetLayers(image, 25, 6);
 
             Console.WriteLine(Part1(layers));
-
+            PrintImage(RenderImage(layers), 25, 6);
         }
     }
 }
