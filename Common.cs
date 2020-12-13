70a0eea0c725ec4129830e68a78e7ed0c3a9bde9
Matt Ferlo <matt.ferlo@gmail.com>
2020-12-13 12:03:41 -0800

    Common.Product

--- Common.cs
+++ Common.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,5 +15,14 @@ namespace Advent2020
         static Regex IntRegex = new Regex(@"-?\d+");
         static IList<int> IntLine(string line) =>
             IntRegex.Matches(line).Select(match => int.Parse(match.Value)).ToList();
+        
+        public static long Product(this IEnumerable<int> numbers) => numbers.Product(x => (long)x);
+        public static long Product(this IEnumerable<long> numbers) => numbers.Product(x => x);
+
+        public static long Product<T>(this IEnumerable<T> numbers, Func<T, int> projection) =>
+            Product(numbers, item => (long)projection(item));
+
+        public static long Product<T>(this IEnumerable<T> numbers, Func<T, long> projection) =>
+            numbers.Aggregate(1L, (total, current) => total *= projection(current));
     }
 }
